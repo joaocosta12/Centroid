ae7b24f879d52ad13db40ebc2f9b5e9484d284d2
joaocosta12 <joao_rln27@hotmail.com>
2018-08-24 17:53:45 -0300

    First commit

---
+++ CMakeFiles/Centroid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Centroid.dir/src/main.cpp.o"
+  "Centroid.pdb"
+  "Centroid"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Centroid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
